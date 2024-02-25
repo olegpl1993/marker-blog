508dd72697646859cde3dce12741086fd76710d5
Oleg <olegpl1993@gmail.com>
2024-02-25 23:00:57 +0200

    refactor: create router to file

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,9 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import Blog from "./pages/Blog/Blog";
-import Topic from "./pages/Topic/Topic";
+import router from "./router";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Blog />,
-      },
-      {
-        path: "topic/:id",
-        element: <Topic />,
-      },
-      {
-        path: "/:category",
-        element: <Blog />,
-      },
-    ],
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
