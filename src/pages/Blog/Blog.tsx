a988ea66241429683ec1d1b9e535f05824343dc2
Oleg <olegpl1993@gmail.com>
2024-03-07 21:26:11 +0200

    refactor: fetch posts

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams, useSearchParams } from "react-router-dom";
+import { fetchCategories } from "../../api/categories";
 import { fetchPosts } from "../../api/posts";
 import { fetchPostsByCategory } from "../../api/postsByCategory";
 import Spinner from "../../components/Spinner/Spinner";
@@ -13,29 +14,46 @@ function Blog() {
   const [searchParams] = useSearchParams();
   const queryValue = searchParams.get("search");
 
-  const query = useQuery({
-    queryKey: ["posts", category, queryValue],
-    queryFn: category
-      ? () => fetchPostsByCategory(category, queryValue)
-      : () => fetchPosts(queryValue),
+  const queryCategories = useQuery({
+    queryKey: ["categories", category],
+    queryFn: () => fetchCategories(),
+    enabled: !!category,
   });
 
-  if (query.isLoading)
+  const categoryID = queryCategories.data?.find(
+    (item) => item.slug === category
+  )?.id;
+
+  const queryPosts = useQuery({
+    queryKey: ["posts", queryValue, category],
+    queryFn: () => fetchPosts(queryValue),
+    enabled: !category,
+  });
+
+  const queryCategoryPosts = useQuery({
+    queryKey: ["categoryPosts", category, queryValue, categoryID],
+    queryFn: () => fetchPostsByCategory(categoryID!, queryValue),
+    enabled: !!categoryID,
+  });
+
+  const queryResult = category ? queryCategoryPosts : queryPosts;
+
+  if (queryResult.isLoading)
     return (
       <div className={styles.spinnerBox}>
         <Spinner />
       </div>
     );
 
-  if (query.isError) return <PageNotFound />;
+  if (queryResult.isError) return <PageNotFound />;
 
   return (
     <div className={styles.blog}>
       <div className={styles.content}>
-        {query.data?.length === 0 ? (
+        {queryResult.data?.length === 0 ? (
           <div className={styles.noPosts}>Постів не знайдено</div>
         ) : (
-          query.data?.map((post) => (
+          queryResult.data?.map((post) => (
             <div className={styles.post} key={post.id}>
               <Card post={post} />
             </div>
