25074b58c2d5f8b6cfc9582a7e664f9bca80f8ce
Oleg <olegpl1993@gmail.com>
2024-03-10 23:21:59 +0200

    feat: new logo and meta description

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { fetchCategories } from "../../api/categories";
 import BurgerMenu from "./BurgerMenu/BurgerMenu";
 import styles from "./Header.module.css";
 
 function Header() {
+  const navigate = useNavigate();
   const query = useQuery({
     queryKey: ["categories"],
     queryFn: () => fetchCategories(),
   });
 
+  const handleClick = () => {
+    navigate(new URL(`/`, window.location.origin).pathname);
+  };
+
   return (
     <header className={styles.header}>
-      <Link to="/" className={styles.logo}>
-        Game Marker
-      </Link>
+      <img
+        src="/logo-gamepad.png"
+        alt="logo gamepad"
+        className={styles.logoImg}
+        onClick={handleClick}
+      />
       <BurgerMenu categories={query.data} />
     </header>
   );
