b2311c1a7654b14c7f60d48d0dc45467b151f2e5
Oleg <olegpl1993@gmail.com>
2024-03-24 23:37:41 +0200

    feat: home layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,22 @@
+import { useContext } from "react";
+import Spinner from "../../components/Spinner/Spinner";
+import { CategoryContext } from "../../contexts/CategoryProvider";
+import { TagContext } from "../../contexts/TagProvider";
 import styles from "./Home.module.css";
 import Recommended from "./Recommended/Recommended";
 import Slider from "./Slider/Slider";
 
 export function Home() {
+  const categories = useContext(CategoryContext);
+  const tags = useContext(TagContext);
+
+  if (!categories || !tags)
+    return (
+      <div className={styles.spinnerBox}>
+        <Spinner />
+      </div>
+    );
+
   return (
     <div className={styles.home}>
       <Slider />
