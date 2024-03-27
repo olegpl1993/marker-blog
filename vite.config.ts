9770f89a68a139f3c7ecd55cc0afb97c5d9bd2cd
Oleg <olegpl1993@gmail.com>
2024-03-27 22:04:10 +0200

    fix: vite config for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name][hash].js",
-        chunkFileNames: "[name][hash].js",
-        assetFileNames: "[name][hash].[ext]",
-      },
-    },
-  },
 });
