a5e6e8d23deae1b58c7319a82a525f868bfc4bf6
Oleg <olegpl1993@gmail.com>
2024-04-09 03:41:43 +0300

    fix: sitemapconstructor

--- sitemapConstructor.js
+++ sitemapConstructor.js
@@ -1,18 +1,18 @@
 import fs from "fs";
 
-const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-`;
-
 const data = fs.readFileSync("./urls.txt").toString().split("\n");
-const xmlData = data.reduce(
-  (acc, url) =>
-    acc +
+const linkData = data.map(
+  (url) =>
     `
 <url>
   <loc>${url}</loc>
 </url>`,
-  sitemap
-);
+).join("");
 
-fs.writeFileSync("./dist/sitemap.xml", xmlData);
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${linkData}
+</urlset>
+`;
+
+fs.writeFileSync("./dist/sitemap.xml", sitemap);
