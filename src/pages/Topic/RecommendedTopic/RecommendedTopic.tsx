c03101b76c7056867d75341975926a6fe02dc95e
Oleg <olegpl1993@gmail.com>
2024-04-21 20:38:38 +0300

    feat: improve recomendedTopic

--- src/pages/Topic/RecommendedTopic/RecommendedTopic.tsx
+++ src/pages/Topic/RecommendedTopic/RecommendedTopic.tsx
@@ -15,33 +15,35 @@ const RecommendedTopic = memo((props: Props) => {
 
   const recommendedTopicQuery = useQuery({
     queryKey: ["RecommendedQuery", categories, tags],
-    queryFn: () => fetchPosts(categories, tags, undefined, undefined, 3, id),
+    queryFn: () => fetchPosts(categories, tags, undefined, undefined, 2, id),
     enabled: categories.length > 0 || tags.length > 0,
   });
-  const data = recommendedTopicQuery.data?.data;
+  const recommendedPosts = recommendedTopicQuery.data?.data;
 
   const additionalQuery = useQuery({
-    queryKey: ["AdditionalQuery", data],
+    queryKey: ["AdditionalQuery", recommendedPosts],
     queryFn: () =>
-      fetchPosts(
-        undefined,
-        undefined,
-        undefined,
-        undefined,
-        3 - data!.length,
-        id
-      ),
-    enabled: data && data.length < 3 ? true : false,
+      fetchPosts(undefined, undefined, undefined, undefined, 20, id),
+    enabled: !!recommendedPosts,
   });
+  const randomAdditionalPosts = additionalQuery.data?.data.sort(
+    () => Math.random() - 0.5
+  );
 
   const renderData =
-    data && additionalQuery.data?.data
-      ? [...data, ...additionalQuery.data.data]
-      : data;
+    recommendedPosts &&
+    randomAdditionalPosts &&
+    [...recommendedPosts, ...randomAdditionalPosts]
+      .filter(
+        (post, index, arr) =>
+          index === arr.findIndex((findPost) => findPost.id === post.id)
+      )
+      .slice(0, 3);
 
+  if (!renderData) return null;
   return (
     <div className={styles.recommendedTopic}>
-      <h2 className={styles.title}>Рекомендуемо новини</h2>
+      <span className={styles.title}>Схожі новини</span>
       <div className={styles.row}>
         {renderData &&
           renderData.map((post) => (
